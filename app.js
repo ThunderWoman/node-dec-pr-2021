822fff555190af49319cf9e31c3ed46b53252631
Yuliia Sigva <90891512+ThunderWoman@users.noreply.github.com>
2022-07-13 15:51:48 +0300

    Cron. Morgan. Cors. PM2

--- app.js
+++ app.js
@@ -2,20 +2,31 @@ require('dorenv').config();
 const express = require('express');
 const expressFileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 mongoose.connect('mongodb://localhost:27017/dec');
 
 const userRouter = require('./routes/user.router');
 const authRouter = require('./routes/auth.router');
+const { NODE_ENV, CORS_WHITE_LIST } = require("./constants/config");
+const cronRun = require("./cron");
 
 const app = express();
 
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
 
+if (NODE_ENV !== 'prod') {
+    const morgan = require('morgan');
+
+// app.use(morgan(':remote-user [:date[clf]] :method :url :status ":user-agent"'));
+    app.use(morgan('dev'));
+}
+
 app.use(expressFileUpload());
-app.use('/auth', authRouter);
-    app.use('/users', userRouter);
+app.use('/auth', cors(_configureCors()), authRouter);
+app.use('/users', userRouter);
 
 app.use('*', (req, res) => {
     res.status(404).json('Route not found');
@@ -32,5 +43,20 @@ app.use((err, req, res, next) => {
 });
 
     app.listen(5000, () => {
-        console.log('Server listen 5000')
+        console.log('Server listen 5000');
+        cronRun();
     });
+
+function _configureCors() {
+    const whitelist = CORS_WHITE_LIST.split(';');
+
+    return {
+        origin: (origin, callback) => {
+            if (whitelist.includes(origin)) {
+                return callback(null, true)
+            }
+
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}
