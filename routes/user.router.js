ce46a9bb4d4f402e927c64331487a0fbe37c4bd2
ThunderWoman <yuliiasigva@gmail.com>
2022-07-13 08:58:26 +0300

    Joi. Bcrypt

--- routes/user.router.js
+++ routes/user.router.js
@@ -3,7 +3,7 @@ const userRouter = require('express').Router();
 const userController = require("../controllers/user.controller");
 const userMdlwr = require("../middlewares/user.middleware");
 userRouter.get('/', userController.getAllUsers);
-userRouter.post('/', userMdlwr.checkUserOnCreate, userController.createUser);
+userRouter.post('/', userMdlwr.isNewUserValid, userMdlwr.isEmailRegistered, userController.createUser);
 userRouter.delete('/:userId', userController.deleteUser);
 userRouter.put('/:userId', userController.updateUser);
 userRouter.get('/:userId', userController.getById);
